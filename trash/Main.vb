252ba839f068807884af53d37261b4d69f900158
jenoki <jenoki@gill>
2019-10-17 04:35:13 +0900

    プロジェクト ファイルを追加します。

---
+++ trash/Main.vb
@@ -0,0 +1,23 @@
+﻿Friend Module Main
+	Public Sub Main()
+		'Parse Options
+		Dim Options As String() = Environment.GetCommandLineArgs()
+		Dim TargetFile As String
+		If Options.Length = 1 Then
+			Console.WriteLine("Usage: {0} <file>", arg0:=My.Application.Info.Title)
+			Exit Sub
+		End If
+		TargetFile = Options.Last
+
+		Try
+			My.Computer.FileSystem.DeleteFile(TargetFile,
+		  showUI:=FileIO.UIOption.OnlyErrorDialogs,
+		  recycle:=FileIO.RecycleOption.SendToRecycleBin)
+		Catch ex As Exception
+			Console.WriteLine(ex.Message)
+			Exit Sub
+		End Try
+
+	End Sub
+
+End Module
